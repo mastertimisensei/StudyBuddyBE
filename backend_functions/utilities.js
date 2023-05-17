7e027e545d3fe0fa90b6a1fc20693a9431fc4d36
aamnatayyab6 <126985385+aamnatayyab6@users.noreply.github.com>
2023-05-18 00:50:07 +0200

    removephoto functionality

--- backend_functions/utilities.js
+++ backend_functions/utilities.js
@@ -180,9 +180,11 @@ const setUserData = async (
   console.log(email);
   admin.auth().getUserByEmail(email);
   const userRef = admin.firestore().collection("users").doc(email);
-  await uploadProfilePicture(uid, photoPath);
-  //var photoLink = await getProfilePicture(uid);
-  photoLink = await showProfilePicture(uid);
+  let photoLink = "";
+  if (photoPath !== null) {
+    await uploadProfilePicture(uid, photoPath);
+    photoLink = await showProfilePicture(uid);
+  }
   admin
     .firestore()
     .collection("users")
